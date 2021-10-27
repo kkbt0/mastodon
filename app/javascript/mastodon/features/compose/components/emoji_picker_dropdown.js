7ef140e71d38b9abd8150b52ec183e7780bf3da8
mashirozx <moezhx@outlook.com>
2021-10-27 15:13:35 +0800

    feat: add category name to custom emoji search keywords list

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
@@ -281,7 +281,7 @@ class EmojiPickerMenu extends React.PureComponent {
           showSkinTones={false}
           backgroundImageFn={backgroundImageFn}
           notFound={notFoundFn}
-          autoFocus={false}
+          autoFocus={window.innerWidth > 625}
           emojiTooltip
         />
 
