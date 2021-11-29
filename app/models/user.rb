368e9469b403791f737d10a391bd1fb5ae2a737c
mashirozx <moezhx@outlook.com>
2021-11-29 17:24:50 +0800

    feat: use sans font for chinese
    perf: noto serif option
    
    fix: logic handling setting_system_font_ui
    
    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -129,7 +129,7 @@ class User < ApplicationRecord
            :advanced_layout, :use_blurhash, :use_pending_items, :trends, :crop_images,
            :default_federation, :default_content_type,
            :disable_swiping,
-           :enable_snowfall, :custom_css,
+           :enable_snowfall, :enable_noto_serif, :custom_css,
            to: :settings, prefix: :setting, allow_nil: false
 
   attr_reader :invite_code, :sign_in_token_attempt
