74bf121f029e9518a05570ea808ac15e20a04fe6
mashirozx <moezhx@outlook.com>
2022-03-05 10:08:53 +0800

    fix: db schema

--- db/schema.rb
+++ db/schema.rb
@@ -846,12 +846,12 @@ ActiveRecord::Schema.define(version: 2022_03_10_060959) do
     t.text "spoiler_text", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content_type"
+    t.boolean "local_only"
     t.bigint "ordered_media_attachment_ids", array: true
     t.text "media_descriptions", array: true
     t.string "poll_options", array: true
     t.boolean "sensitive"
-    t.string "content_type"
-    t.boolean "local_only"
     t.index ["account_id"], name: "index_status_edits_on_account_id"
     t.index ["status_id"], name: "index_status_edits_on_status_id"
   end
@@ -894,13 +894,12 @@ ActiveRecord::Schema.define(version: 2022_03_10_060959) do
     t.bigint "in_reply_to_account_id"
     t.bigint "poll_id"
     t.datetime "deleted_at"
-    t.datetime "edited_at"
-    t.boolean "trendable"
-    t.bigint "ordered_media_attachment_ids", array: true
     t.boolean "local_only"
     t.string "content_type"
     t.bigint "quote_id"
     t.datetime "edited_at"
+    t.boolean "trendable"
+    t.bigint "ordered_media_attachment_ids", array: true
     t.index ["account_id", "id", "visibility", "updated_at"], name: "index_statuses_20190820", order: { id: :desc }, where: "(deleted_at IS NULL)"
     t.index ["deleted_at"], name: "index_statuses_on_deleted_at", where: "(deleted_at IS NOT NULL)"
     t.index ["id", "account_id"], name: "index_statuses_local_20190824", order: { id: :desc }, where: "((local OR (uri IS NULL)) AND (deleted_at IS NULL) AND (visibility = 0) AND (reblog_of_id IS NULL) AND ((NOT reply) OR (in_reply_to_account_id = account_id)))"
